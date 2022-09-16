1132614971817e04a60ac04f6c78e5837bd2c5fa
Ythan Zhang <cn.yiki@gmail.com>
2022-09-16 12:02:09 +0800

    Add short alias for numeric types

---
+++ piu_number.h
@@ -0,0 +1,63 @@
+//
+// Created by YthanZhang on 2022/9/16.
+//
+
+#ifndef FANV2ST_PIU_NUMBER_H
+#define FANV2ST_PIU_NUMBER_H
+
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+#define u8 uint8_t
+#define i8 int8_t
+
+#define u16 uint16_t
+#define i16 int16_t
+
+#define u32 uint32_t
+#define i32 int32_t
+
+#define u64 uint64_t
+#define i64 int64_t
+
+#define u8_MAX UINT8_MAX
+#define u8_MIN 0
+
+#define i8_MAX INT8_MAX
+#define i8_MIN INT8_MIN
+
+#define u16_MAX UINT16_MAX
+#define u16_MIN 0
+
+#define i16_MAX INT16_MAX
+#define i16_MIN INT16_MIN
+
+#define u32_MAX UINT32_MAX
+#define u32_MIN 0
+
+#define i32_MAX INT32_MAX
+#define i32_MIN INT32_MIN
+
+#define u64_MAX UINT64_MAX
+#define u64_MIN 0
+
+#define i64_MAX INT64_MAX
+#define i64_MIN INT64_MIN
+
+
+#define f32 float
+#define f64 double
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // FANV2ST_PIU_NUMBER_H
